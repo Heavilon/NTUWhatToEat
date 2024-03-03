237237aa8641f01adc531814c87a00aa93a0460c
Yu, Sheng-Feng <75771174+Heavilon@users.noreply.github.com>
2024-03-03 22:14:06 +0800

    Update 112_What_to_Eat_FnlVersion.py

--- 112_What_to_Eat_FnlVersion.py
+++ 112_What_to_Eat_FnlVersion.py
@@ -12,7 +12,7 @@ db_settings = {
     "host": "127.0.0.1",
     "port": 3306,
     "user": "root",
-    "password": "QntwsB08701215",
+    "password": "********",
     "db": "ntu_what_to_eat",
     "charset": "utf8"
     }
